d4e1f4534a2263bd50a24dfd59811271d487190a
in2dm3-a <in2dm3-a@HZ401029.elorrieta.int>
2024-10-25 09:53:39 +0200

    version 25/10 ventana perfil

--- Reto1-E5/src/Vista/Perfil.java
+++ Reto1-E5/src/Vista/Perfil.java
@@ -24,10 +24,10 @@ public class Perfil extends JPanel {
 		
 		JLabel lblNewLabel = new JLabel(user);
 		lblNewLabel.setFont(new Font("Arial Black", Font.PLAIN, 28));
-		lblNewLabel.setBounds(10, 11, 135, 84);
+		lblNewLabel.setBounds(10, 11, 914, 84);
 		add(lblNewLabel);
 
-		JButton btnAtras = new JButton("Atras");
+		JButton btnAtras = new JButton("Cerrar Sesión");
 		btnAtras.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Login r = new Login();
@@ -40,7 +40,8 @@ public class Perfil extends JPanel {
 				repaint();
 			}
 		});
-		btnAtras.setBounds(822, 444, 89, 23);
+		btnAtras.setFont(new Font("Arial Black", Font.PLAIN, 17));
+		btnAtras.setBounds(570, 297, 182, 46);
 		add(btnAtras);
 		
 		JButton btnWorkouts = new JButton("Ver Workouts");
@@ -57,7 +58,24 @@ public class Perfil extends JPanel {
 			}
 		});
 		btnWorkouts.setFont(new Font("Arial Black", Font.PLAIN, 17));
-		btnWorkouts.setBounds(525, 49, 182, 46);
+		btnWorkouts.setBounds(570, 107, 182, 46);
 		add(btnWorkouts);
+		
+		JButton btnAcciones = new JButton("Modificar Perfil");
+		btnAcciones.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Workouts r = new Workouts(user);
+				r.setSize(950, 500);
+				r.setLocation(0, 0);
+				
+				removeAll();
+				add(r, BorderLayout.CENTER);
+				revalidate();
+				repaint();
+			}
+		});
+		btnAcciones.setFont(new Font("Arial Black", Font.PLAIN, 17));
+		btnAcciones.setBounds(570, 200, 182, 46);
+		add(btnAcciones);
 	}
 }
